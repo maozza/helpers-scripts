9f17fc2785a9f9a89b8a9086a18b0d0f8caf83c8
Maoz Zadok <maozza@gmail.com>
2021-12-13 15:25:03 +0200

    support json output

--- elasticsearch2csv.py
+++ elasticsearch2csv.py
@@ -15,23 +15,30 @@ for example query_string: this AND that OR thus
 parser=argparse.ArgumentParser()
 parser.add_argument("-i", "--index", dest="index",
                     help="required: index name", required=True)
-parser.add_argument("-t", "--type", dest="type",
-                    help="required: doc type name", required=True)
+parser.add_argument("-t", "--type", dest="type", default='_doc',
+                    help="required: doc type name")
 parser.add_argument("--host", dest="host",
                     help="host url default=localhost", default='localhost')
 parser.add_argument("--output", dest="output",
-                    help="output file default: output.csv", default="output.csv")
+                    help="output file default: output", default="output")
 parser.add_argument("-d", "--delimiter", dest="delimiter",
                     help="csv delimiter - default - , ", default=',')
 parser.add_argument("--query", dest="json_query",
-                    help="json customized query if query exists, query_string will be ignored", default=None)
-
+                    help="json customized query if query exists, query_string will be ignored, will also parse query from Kibana inspec", default=None)
+parser.add_argument("--parse_query", dest="parse_query",
+                    help="parse query from Kibana inspect", default=None)
+parser.add_argument("--parse_query_file", dest="parse_query_file",
+                    help="parse query from file, Kibana inspect", default=None)
 parser.add_argument("--query_string", dest="query_string",
                     help="query string \'this AND that OR thus\'", default="*")
 parser.add_argument("--fields", dest="fields",
                     help="comma separated fields - default all", default='all')
 parser.add_argument("--size", dest="size",
                     help="size of the respons, default 1000", default='1000')
+parser.add_argument("-u","--user", dest="user",
+                    help="<user:password> Server user and password", default=None)
+parser.add_argument("-f","--format", dest="format",
+                    help="<csv|json> output file format", default="json")                    
 args = vars(parser.parse_args())
 
 host = args['host']
@@ -43,20 +50,40 @@ delimiter = args['delimiter']
 fields= args['fields']
 size=int(args['size'])
 json_query = args['json_query']
-
-
+parse_query = args['parse_query']
+parse_query_file = args['parse_query_file']
+auth = args['user']
+format = args['format']
 
 '''
 Create elasticsearch instance
 '''
-es = elasticsearch.Elasticsearch(host)
+
+if auth:
+    if ":" in auth:
+        user,password = auth.split(":",1)
+    else:
+        user = auth
+        password = input("Enter host password for user '{}': ".format(user))
+    es = elasticsearch.Elasticsearch(host,http_auth=(user, password))
+else:
+    es = elasticsearch.Elasticsearch(host)
 
 
 '''
 Get query from input
 '''
+if parse_query_file:
+    with open(parse_query_file) as f:
+       parse_query = f.read()
+
 if json_query:
-    query = json.loads(json_query)
+    query = json.loads(json_query)    
+    if "query" in query:
+        query = dict(query =query['query'] )
+elif parse_query:
+    query = json.loads(parse_query)
+    query = dict(query =query['query'] )   
 else:
     query = dict(
         query = dict(
@@ -66,6 +93,7 @@ else:
         )
     )
 
+query['size'] = size
 
 '''
 Get real index name in case index is alias
@@ -80,34 +108,56 @@ for i in aliases:
 '''
 Fetch the mapping in order to create the header
 '''
-mapping=es.indices.get_mapping(index=index,doc_type=doc_type)[index]['mappings'][doc_type]['properties'].keys()
+mapping=es.indices.get_mapping(index=index,doc_type=doc_type,include_type_name=True)[index]['mappings'][doc_type]['properties'].keys()
 
 '''
 Set handler to elasticsearch
 '''
-scanResp= helpers.scan(client=es, query=query, scroll= "10m", index=index,size=size, doc_type=doc_type, clear_scroll=False, request_timeout=300)
+scanResp= helpers.scan(client=es, query=query, scroll="10m", index=index,size=size, doc_type=doc_type, clear_scroll=True, request_timeout=300)
 
 
+if format == "csv":
+    if not output_files.endswith(".csv"):
+        output_files += ".csv" 
+    with open(output_files, 'w') as f:
+        counter = 0
+        if fields == "all":
+            w = csv.DictWriter(f, mapping, delimiter=delimiter,extrasaction='ignore',quoting=csv.QUOTE_MINIMAL)
+            w.writeheader()
+        else:
+            fields = fields.split(",")
+            w = csv.DictWriter(f, [i for i in mapping if i in fields], delimiter=delimiter, extrasaction='ignore',quoting=csv.QUOTE_MINIMAL )
+            w.fieldnames = fields
+            w.writeheader()
+        try:
+            for row in scanResp:
+                if counter >= size:
+                    break;
+                _ = w.writerow(row['_source'])
+                counter +=1
+        except elasticsearch.exceptions.NotFoundError:
+            pass
+        except elasticsearch.exceptions.RequestError:
+            pass
 
-with open(output_files, 'w') as f:
-    counter = 0
-    if fields == "all":
-        w = csv.DictWriter(f, mapping, delimiter=delimiter,quoting=csv.QUOTE_MINIMAL)
-    else:
-        fields = fields.split(",")
-        w = csv.DictWriter(f, [i for i in mapping if i in fields], delimiter=delimiter, extrasaction='ignore',quoting=csv.QUOTE_MINIMAL )
-    w.writeheader()
-    try:
-        for row in scanResp:
-            _ = w.writerow(row['_source'])
-            counter +=1
-    except elasticsearch.exceptions.NotFoundError:
-        pass
-    except elasticsearch.exceptions.RequestError:
-        pass
+if format == "json":
+    if not output_files.endswith(".json"):
+        output_files += ".json"     
+    with open(output_files, 'w') as f:
+        counter = 0
+        try:
+            for row in scanResp:
+                if counter >= size:
+                    break;
+                _ = f.write(json.dumps(row['_source'])+"\n")
+                counter +=1
+        except elasticsearch.exceptions.NotFoundError:
+            pass
+        except elasticsearch.exceptions.RequestError:
+            pass            
 
 if counter > 0:
-    print('%s lines was expotred to file: %s'%(counter,output_files))
+    print('%s lines was exportred to file: %s'%(counter,output_files))
 else:
     if len(host.split(':')) == 1:
         host +=':9200'
